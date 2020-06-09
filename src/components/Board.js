23f5ebb2f2f69d9bad5570bcf8336c0b7495ca9b
amandena <manda.panda91@yahoo.com>
2020-06-09 17:01:05 -0400

    created stateful board component

---
+++ src/components/Board.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import BoardBox from './BoardBox'
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      boxes: Array(9).fill(null),
+      history: [],
+      xIsNext: true
+    }
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default Board
