aba0cd721b466759f01eed2c681b30ff0f516dac
amandena <manda.panda91@yahoo.com>
2020-06-10 15:57:43 -0400

    added 9 box components for each spot on the board with values

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import BoardBox from './BoardBox'
 import * as utils from '../utils/helperFunctions'
 
+
 class Board extends React.Component {
   constructor(props) {
     super(props)
@@ -15,8 +16,28 @@ class Board extends React.Component {
 
   render() {
     return(
-      <div>
-        <BoardBox/>
+      <div className='board-wrapper'>
+        <div className='board'>
+          <div className='board-row'>
+            <BoardBox value={this.state.boxes[0]}/>
+            <BoardBox value={this.state.boxes[1]}/>
+            <BoardBox value={this.state.boxes[2]}/>
+          </div>
+
+          <div className='board-row'>
+            <BoardBox value={this.state.boxes[3]}/>
+            <BoardBox value={this.state.boxes[4]}/>
+            <BoardBox value={this.state.boxes[5]}/>
+          </div>
+
+          <div className='board-row'>
+            <BoardBox value={this.state.boxes[6]}/>
+            <BoardBox value={this.state.boxes[7]}/>
+            <BoardBox value={this.state.boxes[8]}/>
+          </div>
+        </div>
+      
+        
       </div>
     )
   }
