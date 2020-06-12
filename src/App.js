f18c6c312456bfc6952d9ce6641bb92820027923
amandena <manda.panda91@yahoo.com>
2020-06-11 22:00:51 -0400

    imported scoreboard component and new stylesheet

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './App.css'
 import './styles/board.css'
 import './styles/board-box.css'
+import './styles/buttons.css'
 import Board from './components/Board'
+import Scoreboard from './components/Scoreboard'
 
 function App() {
   return (
     <div className='App'>
       <h1>TIC TAC TOE</h1>
       <Board/>
+      <Scoreboard/>
     </div>
   )
 }
