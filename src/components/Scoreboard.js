9f52578a1b4ac0ddb391d44ce99e592d27e64303
amandena <manda.panda91@yahoo.com>
2020-06-12 18:50:29 -0400

    completed build of scoreboard component

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -7,11 +7,27 @@ class Scoreboard extends React.Component {
     scoreboard: []
   }
 
+  // load data from local storage and update component state once component mounts
+  async componentDidMount() {
+    let storage = await new Storage().getData()
+     this.setState({
+       scoreboard: storage
+     })
+  }
+
   render() {
     return(
       <div className='game'>
         <h1>Recent Games:</h1>
+        <ul>
+          {this.state.scoreboard.map((leader, key) => {
+            return <li key={key}>leader</li>  
+          })}
+        </ul>
 
+        <Link to='/board'>
+          <button className='btn'>Start New Game</button>
+        </Link>
       </div>
     )
   }
